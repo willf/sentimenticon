0f1a49bbaa8f94094d191748712cce5afa819996
Will Fitzgerald <will@wordnik.com>
2015-04-20 16:39:54 -0400

    add data files to setup

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
+import os
 from setuptools import setup, find_packages
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(name='Sentimenticon',
+      version="0.0.1",
+      author="Will Fitzgerald",
+      author_email="will.fitzgerald@pobox.com",
+      description="Word-based sentiment analyzer",
+      license="BSD",
+      keywords="nlp sentiment",
+      long_description=read("README.md"),
       packages=find_packages(),
       package_data={'': ['*.txt']})
