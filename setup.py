7b20a379d3062303c5a81c9f03e5a79fc8881e93
Will Fitzgerald <will@wordnik.com>
2015-04-20 16:47:54 -0400

    format

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 import os
+
 from setuptools import setup, find_packages
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(name='Sentimenticon',
       version="0.0.1",
       author="Will Fitzgerald",
       author_email="will.fitzgerald@pobox.com",
       description="Word-based sentiment analyzer",
+      url='http://github.com/willf/sentimenticon',
       license="BSD",
       keywords="nlp sentiment",
       long_description=read("README.md"),
